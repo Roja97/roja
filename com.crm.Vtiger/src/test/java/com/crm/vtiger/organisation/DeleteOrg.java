5f9c3b953da49499b4b5da0beb60b7a5b5aec73a
Admin <Admin@DESKTOP-AS9VME5>
2022-03-26 14:12:16 +0530

    org is deleted

---
+++ com.crm.Vtiger/src/test/java/com/crm/vtiger/organisation/DeleteOrg.java
@@ -0,0 +1,10 @@
+package com.crm.vtiger.organisation;
+
+import org.testng.annotations.Test;
+
+public class DeleteOrg {
+@Test
+public void deleteOrg() {
+	System.out.println("org is deleted");
+}
+}
