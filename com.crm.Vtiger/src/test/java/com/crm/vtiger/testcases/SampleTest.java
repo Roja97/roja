a63059629371adaf9d4a9ab33e6d86b6961fdc55
Admin <Admin@DESKTOP-AS9VME5>
2022-03-28 16:10:14 +0530

    sample test1 is created

---
+++ com.crm.Vtiger/src/test/java/com/crm/vtiger/testcases/SampleTest.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void sampleTest() {
+		System.out.println("Sample test case");
+	}
+
+}
