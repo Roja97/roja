d6e1eca5114176a8d0ed8b2ca79080a6a650c9c6
Admin <Admin@DESKTOP-AS9VME5>
2022-03-28 15:01:33 +0530

    new is created

---
+++ com.crm.Vtiger/src/test/java/com/crm/vtiger/testcases/NewTest.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.testcases;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void newTest() {
+		System.out.println("test is created");
+	}
+
+}
